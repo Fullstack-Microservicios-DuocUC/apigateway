b38071462f6d98ad12f3f007e7a9b4300b51178a
zBleend <juancrichile@gmail.com>
2026-06-03 18:39:18 -0400

    Respaldo y modificacion de ApiGateway

---
+++ src/main/java/cl/duoc/mineria/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package cl.duoc.mineria.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+		System.out.println("API Gateway levantado en el puerto 8080");
+	}
+
+}
